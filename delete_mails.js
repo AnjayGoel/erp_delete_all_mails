22de6adf11083dcf0257e5075a1df3ec33204487
AnjayGoel <angoel123@gmail.com>
2019-04-23 00:27:30 +0530

    Added Script File.

---
+++ delete_mails.js
@@ -0,0 +1,19 @@
+params = 'actionType=inbox&limit=10000&offset=0'
+var params2="actionType=Delete&items="
+xmlHttp=new XMLHttpRequest();
+xmlHttp.open("POST",'https://erp.iitkgp.ac.in/IMail/emailAjax.jsp',false);
+xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xmlHttp.send(params);
+if (xmlHttp.readyState==4 && xmlHttp.status==200) {
+        var ele = document.createElement('html')
+        ele.innerHTML=xmlHttp.responseText
+        var ids = ele.getElementsByTagName('div')[5].getElementsByTagName("tr")
+        params2+=ids[0].id
+        for (let i=1;i<4;i++) {
+            params2+=','+ids[i].id
+        }
+        xmlHttp.open("POST",'https://erp.iitkgp.ac.in/IMail/emailAjax.jsp',false);
+        xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xmlHttp.send(params2);
+        alert("Done");
+}
